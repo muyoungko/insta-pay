85b33473a3092f2c0f9b69024ae2113ff517951e
Muyoung Ko <muyoungko@gmail.com>
2018-05-24 23:36:40 +0900

    중간 커밋

--- App.js
+++ App.js
@@ -1,16 +1,76 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import {
+  View,
+  Text,
+  Button,
+  ActivityIndicator,
+  NativeModules,
+  Animated,
+  TouchableWithoutFeedback,
+  StyleSheet
+} from 'react-native';
+import { createStackNavigator} from 'react-navigation';
+import LoginScreen from './src/Login.js';
+
+function __translate(term) {
+
+  //check if localeIdentifier exists
+  if (NativeModules.I18nManager.localeIdentifier) {
+
+    const i18nLocale = NativeModules.I18nManager.localeIdentifier;
+
+    console.log(i18nLocale);
+
+    //Check if has registered terms in current i18nLocale;
+    if (TERMS[i18nLocale]){
+
+      //Return the registered or empty string to prevent error
+      return TERMS[i18nLocale][term] || '';
+
+    } else {
+
+      //Check if has language without a especific region like
+      //Example en-CA to en
+      const simplei18nLocale = i18nLocale.split('_')[0];
+
+      return TERMS[simplei18nLocale][term] || '';
+
+    }
+  }
+}
 
 class HomeScreen extends React.Component {
+  constructor()
+  {
+    super();
+    this.state = {test : 'asdfsdfs'};
+    console.log('test');
+  }
+
+  componentDidMount()
+  {
+    this.setState({
+          test: 'false'
+        }, function(){
+
+        });
+  }
+
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Text>{this.state.test}</Text>
         <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
         />
+        <ActivityIndicator size="large" color="#0000ff" />
       </View>
     );
   }
@@ -18,7 +78,13 @@ class HomeScreen extends React.Component {
 
 
 class DetailsScreen extends React.Component {
+
+  static navigationOptions = {
+    title: '상품상세',
+  };
+
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
@@ -42,17 +108,19 @@ class DetailsScreen extends React.Component {
 
 const MainStackNavigator = createStackNavigator(
   {
+    Login: LoginScreen,
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Details: DetailsScreen
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Login',
   }
 );
 
 
+
 export default class App extends React.Component {
   render() {
-    return <MainStackNavigator />;
+    return <LoginScreen/>;
   }
 }
---
+++ src/Login.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, Text, Button, WebView} from 'react-native';
+import {NavigationActions} from 'react-navigation';
+import url from 'url';
+import queryString from 'query-string';
+
+class LoginScreen extends React.Component {
+  constructor()
+  {
+    super();
+    this.state = {test : 'asdfsdfs'};
+    console.log('test');
+  }
+
+  componentDidMount()
+  {
+    this.setState({
+          test: 'false'
+        }, function(){
+
+        });
+  }
+
+  static navigationOptions = {
+    title: '인스타 연동을 해주세요~',
+  };
+
+  render() {
+
+    return (
+      <WebView
+        source={{uri: 'https://www.instagram.com/oauth/authorize/?client_id=c99f61f0de284159a05576d4b34005bc&redirect_uri=http%3A%2F%2Fwww.sering.co.kr%2Ftalkin%3Ffrom%3Dapp&response_type=code'}}
+        onNavigationStateChange={this._onNavigationStateChange.bind(this)}
+        javaScriptEnabled = {true}
+        domStorageEnabled = {true}
+        injectedJavaScript = {this.state.cookie}
+        startInLoadingState={true}
+        style={{marginTop:20}}
+      />
+    );
+  }
+
+  /**
+    {
+    canGoBack: bool,
+    canGoForward: bool,
+    loading: bool,
+    target: number,
+    title: string,
+    url: string,
+  }*/
+  _onNavigationStateChange(webViewState){
+    var urlObject = url.parse(webViewState.url);
+    //let outUrlString = urlObject.protocol + '//' + urlObject.host + '/more/jump?jump_path=' + encodeURIComponent(urlObject.pathname);
+
+    //alert(urlObject.pathname);
+    var params = queryString.parse(urlObject.query);
+    if(urlObject.pathname == '/talkin' && params.code)
+    {
+      alert('성공');
+    }
+    console.log(webViewState.url);
+  }
+}
+
+
+export default LoginScreen;
